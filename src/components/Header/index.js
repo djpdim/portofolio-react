ce1afe43781dd86dc182e5d2e943b1b17655a6f9
Dimosthenis Pagkas <dimosthenispagkas@MacBook-Pro-3.fios-router.home>
2022-07-17 14:12:06 -0400

    new code for the rest of the portofolio

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,1 +1,29 @@
-import React from "react"
+import React, { Component } from "react"
+import { HashRouter, Redirect, Route } from "react-router-dom"
+import About from "../../components/About"
+import Contact from "../../components/Contact"
+import Navigation from "../../components/Navigation"
+import Portfolio from "../../components/Portfolio"
+import Resume from "../../components/Resume"
+
+class Header extends Component {
+    render() {
+        return (
+            <HashRouter>
+                <header>
+                    <Navigation />
+                </header>
+
+                <div className="content">
+                    <Route exact path="/" render={() => <Redirect to="/about" />} />
+                    <Route path="/about" component={About} />
+                    <Route path="/portfolio" component={Portfolio} />
+                    <Route path="/contact" component={Contact} />
+                    <Route path="/resume" component={Resume} />
+                </div>
+            </HashRouter>
+        )
+    }
+}
+
+export default Header
