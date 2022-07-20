307ccf878f7523bf13076bbf10021d24024c107f
Dimosthenis Pagkas <dimosthenispagkas@MacBook-Pro-3.local>
2022-07-20 18:45:36 -0400

    Updates

---
+++ precache-manifest.b784bcc6001d97e70823babff9d5eb11.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f96c0d7bc34eac617f8ef6a5e5999086",
+    "url": "/portofolio-react/index.html"
+  },
+  {
+    "revision": "7f3453f410f965f5b6ff",
+    "url": "/portofolio-react/static/css/2.1ebb0010.chunk.css"
+  },
+  {
+    "revision": "f1916d9d7ee6a87997b7",
+    "url": "/portofolio-react/static/css/main.fc3d2567.chunk.css"
+  },
+  {
+    "revision": "7f3453f410f965f5b6ff",
+    "url": "/portofolio-react/static/js/2.0ba3ab1d.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "/portofolio-react/static/js/2.0ba3ab1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1916d9d7ee6a87997b7",
+    "url": "/portofolio-react/static/js/main.7e9c0b2d.chunk.js"
+  },
+  {
+    "revision": "977c16bd6b261ddd82a1",
+    "url": "/portofolio-react/static/js/runtime-main.05e9348c.js"
+  },
+  {
+    "revision": "7519ccd6f7c0e985ed5e5014cb1f4316",
+    "url": "/portofolio-react/static/media/Caveat-VariableFont_wght.7519ccd6.ttf"
+  },
+  {
+    "revision": "fa142efff0d6c1c3d131673c3e498f6d",
+    "url": "/portofolio-react/static/media/Dimo.fa142eff.png"
+  },
+  {
+    "revision": "c5c5707cc06cdc898d10dfa2d90be84d",
+    "url": "/portofolio-react/static/media/Dimos Pagkas Resume.c5c5707c.pdf"
+  }
+]);
